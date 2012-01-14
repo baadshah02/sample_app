2854f2c4e3698309af541f30c81cf3d79ad46e9c
Jay Parekh <j.parekh02@gmail.com>
2012-01-14 18:35:41 -0500

    made some changes to user_example.rb

--- user_example.rb
+++ user_example.rb
@@ -4,10 +4,15 @@ class User
 	def initialize(attributes = {})
 	    @name  = attributes[:name]
 	    @email = attributes[:email]
+
+
 	end
 
 	def formatted_email
     	"#{@name} <#{@email}>"
+
+
+
   	end
 
 end
